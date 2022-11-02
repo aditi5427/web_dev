be5b204d896490c758c2d72fe776389b6a01b225
unknown <aditisuman5480@gmail.com>
2022-11-03 00:05:13 +0530

    added

---
+++ jquery/nav.js
@@ -0,0 +1,25 @@
+$('.option').hover(function () {
+   if ($('.list').hasClass("showList")) {
+      $('.list').removeClass("showList")
+   } else {
+      $('.list').addClass("showList")
+   }
+});
+
+$('.option-1').hover(function () {
+   if ($('.list-1').hasClass("showList")) {
+      $('.list-1').removeClass("showList")
+   } else {
+      $('.list-1').addClass("showList")
+   }
+});
+
+$('.option-2').hover(function () {
+   if ($('.list-2').hasClass("showList")) {
+      $('.list-2').removeClass("showList")
+   } else {
+      $('.list-2').addClass("showList")
+   }
+});
+
+
